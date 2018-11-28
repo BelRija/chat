3c62804439d88437fe3bb4acef17e85a73136231
Marija <sharpeima@gmail.com>
2018-11-28 13:35:55 +0100

    test mood color

--- public/main.js
+++ public/main.js
@@ -110,7 +110,7 @@ $(function () {
           //show new message in chat
         $('#messages').append($('<li>')
             .append($('<p id="from">'+'['+new Date()
-                .toLocaleString().split(",")[1]+'] '+msg.uname+": "+'<b>'+msg.message+'</b>'+'</p>'+'[ '+msg.mood+" ]"+'<b>')));
+                .toLocaleString().split(",")[1]+'] '+msg.uname+": "+'<b>'+msg.message+'</b>'+'</p>'+'<p id="mood">[ '+msg.mood+" ]"+'</p>')));
           
         //auto-scroll to bottom
         window.scrollTo(0, document.getElementById('chat').scrollHeight);
