a35ee635491e5c6bf6fd730583e7621d4fdcc2a3
Marija <sharpeima@gmail.com>
2018-11-28 08:45:46 +0100

    aaaaaaaaaaaaaaa

--- index.js
+++ index.js
@@ -225,7 +225,7 @@ function createToneRequest (request) {
 	   toneChatRequest.utterances.push(utterance);
 	 }
   }
-
+  io.sockets.emit('chat message', toneChatRequest);  
   return toneChatRequest;
 }
 
